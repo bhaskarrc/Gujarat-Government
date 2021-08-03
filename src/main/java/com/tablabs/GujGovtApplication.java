a39858c86a1b948ce758cf00a2a4391f63653123
bhaskarrc <bhaskaroychowdhury@gmail.com>
2021-08-03 15:43:48 +0530

    Initial commit

---
+++ src/main/java/com/tablabs/GujGovtApplication.java
@@ -0,0 +1,13 @@
+package com.tablabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GujGovtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GujGovtApplication.class, args);
+	}
+
+}
