52be3621dc0e1b033c441f844d50c4b09eb858b8
bhaskarrc <bhaskaroychowdhury@gmail.com>
2021-08-05 15:22:25 +0530

    Updated PUT mapping functionality

--- src/main/java/com/tablabs/controller/InwardController.java
+++ src/main/java/com/tablabs/controller/InwardController.java
@@ -1,6 +1,7 @@
 package com.tablabs.controller;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,11 +38,9 @@ public class InwardController {
 
 	@PutMapping("/update/{id}")
 	public void updateEntry(@RequestBody Map<String, String> inwardEntry, String id) {
-		Inward_Entry fetchInwardEntry;
-
+		Inward_Entry fetchInwardEntry = service.getInwardEntry(id);
+		
 		if (id != null) {
-			fetchInwardEntry = service.getInwardEntry(id);
-
 			if (inwardEntry.get("inward_no") != null
 					&& Long.parseLong(inwardEntry.get("inward_no")) != fetchInwardEntry.getInward_no()) {
 				fetchInwardEntry.setInward_no(Long.parseLong(inwardEntry.get("inward_no")));
@@ -51,8 +50,99 @@ public class InwardController {
 					&& !inwardEntry.get("inward_dt").equals(fetchInwardEntry.getInward_dt().toString())) {
 				fetchInwardEntry.setInward_dt(LocalDate.parse(inwardEntry.get("inward_dt")));
 			}
-			
+
+			if (inwardEntry.get("letter_type_id") != null
+					&& Integer.parseInt(inwardEntry.get("letter_type_id")) != fetchInwardEntry.getLetter_type_id()) {
+				fetchInwardEntry.setLetter_type_id(Integer.parseInt(inwardEntry.get("letter_type_id")));
+			}
+
+			if (inwardEntry.get("letter_type") != null
+					&& !inwardEntry.get("letter_type").equals(fetchInwardEntry.getLetter_type())) {
+				fetchInwardEntry.setLetter_type(inwardEntry.get("letter_type"));
+			}
+
+			if (inwardEntry.get("letter_no") != null
+					&& !inwardEntry.get("letter_no").equals(fetchInwardEntry.getLetter_no())) {
+				fetchInwardEntry.setLetter_no(inwardEntry.get("letter_no"));
+			}
+
+			if (inwardEntry.get("letter_dt") != null
+					&& !inwardEntry.get("letter_dt").equals(fetchInwardEntry.getLetter_dt().toString())) {
+				fetchInwardEntry.setLetter_dt(LocalDate.parse(inwardEntry.get("letter_dt")));
+			}
+
+			if (inwardEntry.get("letter_details") != null
+					&& !inwardEntry.get("letter_details").equals(fetchInwardEntry.getLetter_details())) {
+				fetchInwardEntry.setLetter_details(inwardEntry.get("letter_details"));
+			}
+
+			if (inwardEntry.get("from_where_details") != null
+					&& !inwardEntry.get("from_where_details").equals(fetchInwardEntry.getFrom_where_details())) {
+				fetchInwardEntry.setFrom_where_details(inwardEntry.get("from_where_details"));
+			}
+
+			if (inwardEntry.get("doi_branch_id") != null
+					&& Long.parseLong(inwardEntry.get("doi_branch_id")) != fetchInwardEntry.getDoi_branch_id()) {
+				fetchInwardEntry.setDoi_branch_id(Long.parseLong(inwardEntry.get("doi_branch_id")));
+			}
+
+			if (inwardEntry.get("doi_employee_id") != null
+					&& Long.parseLong(inwardEntry.get("doi_employee_id")) != fetchInwardEntry.getDoi_employee_id()) {
+				fetchInwardEntry.setDoi_employee_id(Long.parseLong(inwardEntry.get("doi_employee_id")));
+			}
+
+			if (inwardEntry.get("doi_employee_name") != null
+					&& !inwardEntry.get("doi_employee_name").equals(fetchInwardEntry.getDoi_employee_name())) {
+				fetchInwardEntry.setDoi_employee_name(inwardEntry.get("doi_employee_name"));
+			}
+
+			if (inwardEntry.get("reference_no") != null
+					&& !inwardEntry.get("reference_no").equals(fetchInwardEntry.getReference_no())) {
+				fetchInwardEntry.setReference_no(inwardEntry.get("reference_no"));
+			}
+
+			if (inwardEntry.get("reference_dt") != null
+					&& !inwardEntry.get("reference_dt").equals(fetchInwardEntry.getReference_dt().toString())) {
+				fetchInwardEntry.setReference_dt(LocalDateTime.parse(inwardEntry.get("reference_dt")));
+			}
+
+			if (inwardEntry.get("active_status") != null
+					&& Integer.parseInt(inwardEntry.get("active_status")) != fetchInwardEntry.getActive_status()) {
+				fetchInwardEntry.setActive_status(Integer.parseInt(inwardEntry.get("active_status")));
+			}
+
+			if (inwardEntry.get("created_date") != null
+					&& !inwardEntry.get("created_date").equals(fetchInwardEntry.getCreated_date().toString())) {
+				fetchInwardEntry.setCreated_date(LocalDateTime.parse(inwardEntry.get("created_date")));
+			}
+
+			if (inwardEntry.get("created_by") != null
+					&& Long.parseLong(inwardEntry.get("created_by")) != fetchInwardEntry.getCreated_by()) {
+				fetchInwardEntry.setCreated_by(Long.parseLong(inwardEntry.get("created_by")));
+			}
+
+			if (inwardEntry.get("created_by_post") != null
+					&& Long.parseLong(inwardEntry.get("created_by_post")) != fetchInwardEntry.getCreated_by_post()) {
+				fetchInwardEntry.setCreated_by_post(Long.parseLong(inwardEntry.get("created_by_post")));
+			}
+
+			if (inwardEntry.get("updated_date") != null
+					&& !inwardEntry.get("updated_date").equals(fetchInwardEntry.getUpdated_date().toString())) {
+				fetchInwardEntry.setUpdated_date(LocalDateTime.parse(inwardEntry.get("updated_date")));
+			}
+
+			if (inwardEntry.get("updated_by") != null
+					&& Long.parseLong(inwardEntry.get("updated_by")) != fetchInwardEntry.getUpdated_by()) {
+				fetchInwardEntry.setUpdated_by(Long.parseLong(inwardEntry.get("updated_by")));
+			}
+
+			if (inwardEntry.get("updated_by_post") != null
+					&& Long.parseLong(inwardEntry.get("updated_by_post")) != fetchInwardEntry.getUpdated_by_post()) {
+				fetchInwardEntry.setUpdated_by_post(Long.parseLong(inwardEntry.get("updated_by_post")));
+			}
 		}
+
+		service.updateInwardEntry(fetchInwardEntry);
 	}
 
 	@DeleteMapping("/delete/{id}")
